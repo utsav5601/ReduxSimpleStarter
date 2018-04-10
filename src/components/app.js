d95358e1bbf6222f56b9a92e55b8432db9ddb9c7
utsav <utsav@naukri.com>
2018-04-10 17:30:11 +0530

    initial setup for youtube site......

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				
+			</div>
+
+		);
+	}
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDom from 'react-dom';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyDzdd-cUkipk_gQJHKLIGD2LaD4T3TXFuM';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    )
+}
+
+ReactDom.render(<App/>, document.querySelector('.container'));
+
